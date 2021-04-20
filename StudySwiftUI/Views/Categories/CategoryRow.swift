d7d02a4f4eac0ff1286eac9e3f41cbc0262d0faf
money <aimangning@163.com>
2021-04-20 22:01:53 +0800

    adding category and tabbar feature

--- StudySwiftUI/Views/Categories/CategoryRow.swift
+++ StudySwiftUI/Views/Categories/CategoryRow.swift
@@ -12,8 +12,25 @@ struct CategoryRow: View {
     var items: [Landmark]
     
     var body: some View {
-        Text(categoryName)
-            .font(.headline)
+        VStack(alignment: .leading) {
+            Text(categoryName)
+                .font(.headline)
+                .padding(.leading, 15)
+                .padding(.top, 5)
+            
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(alignment: .top, spacing: 0) {
+                    ForEach(items) { landmark in
+                        NavigationLink(
+                            destination: LandmarkDetail(landmark: landmark)
+                            ) {
+                            CategoryItem(landmark: landmark)
+                        }
+                    }
+                }
+            }
+            .frame(height: 185)
+        }
     }
 }
 
@@ -21,6 +38,6 @@ struct CategoryRow_Previews: PreviewProvider {
     static let landmarks = ModelData().landmarks
     
     static var previews: some View {
-        CategoryRow(categoryName: landmarks[0].category.rawValue, items: Array(landmarks.prefix(3)))
+        CategoryRow(categoryName: landmarks[0].category.rawValue, items: Array(landmarks.prefix(4)))
     }
 }
